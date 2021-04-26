561e2756abd5201e4cc1a5d3131571befd8f2dfd
rodrigoknob <rodrigoknob@partithura.com.br>
2021-04-26 14:51:10 -0300

    iniciando a aplicacao

---
+++ src/views/cliente.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import Card from '../components/card'
+
+class Login extends React.Component {
+
+  gerarSenha = (tipo) => {
+    console.log('senha gerada', tipo)
+  }
+
+  render () {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6" style={ {position: 'relative', left: '300px'} }>
+            
+              <div className="bs-docs-section">
+              <Card title="Senha Atual">
+                <div className="row">
+                    <div className="col-lg-6 d-flex justify-content-center">
+                      OI
+                    </div>
+                </div>
+              </Card>
+            </div>
+            <br/>
+            <div className="bs-docs-section">
+              
+              <Card title="Gerar senha">
+                <div className="row">
+                    <div className="col-lg-6 d-flex justify-content-center">
+                      <div className="bs-component">
+                        <button onClick={() =>  this.gerarSenha('N')} className="btn btn-success">Normal</button>
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6">
+                      <div className="bs-component d-flex justify-content-center">
+                        <button onClick={() =>  this.gerarSenha('P')} className="btn btn-info">Preferencial</button>
+                      </div>
+                    </div>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </div>  
+      </div>
+
+    )
+  }
+}
+
+export default Login
